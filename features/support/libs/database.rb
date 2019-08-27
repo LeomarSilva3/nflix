240b74a0d9b44457fc64b58980a464690a05f3b4
Leomar da Silva Barbosa <leomar.barbosa@kaiostech.com>
2019-08-27 17:59:05 -0400

    update

--- features/support/libs/database.rb
+++ features/support/libs/database.rb
@@ -1,6 +1,6 @@
 require "pg"
 
-class DataBase
+class Database
     def delete_movie(title)
         connection = PG.connect(host: "localhost", dbname: "nflix", user: "postgres", password: "qaninja")
         connection.exec("DELETE from public.movies where title = '#{title}'")
