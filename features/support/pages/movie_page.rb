6da2cc7baf34e8998a4dc4214c4abfd13dc675b6
Leomar da Silva Barbosa <leomar.barbosa@kaiostech.com>
2019-08-29 16:28:09 -0400

    update

--- features/support/pages/movie_page.rb
+++ features/support/pages/movie_page.rb
@@ -1,6 +1,11 @@
 class MoviePage 
     include Capybara::DSL
      
+    #construtor
+
+    def initialize
+        @movie_list_css = 'table tbody tr'
+    end
 
     def form
         MovieAdd.new
@@ -26,12 +31,12 @@ class MoviePage
 
     #verifica não ter o filme no catalogo
     def has_no_movie(title)
-        page.has_no_css?('table tbody tr', text: title)
+        page.has_no_css?(@movie_list_css, text: title)
     end
 
     #verifica não ter o filme no catalogo
     def has_movie(title)
-        page.has_css?('table tbody tr', text: title)
+        page.has_css?(@movie_list_css, text: title)
     end
 
     
