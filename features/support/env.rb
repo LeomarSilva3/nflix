643379a0c8e8a6a0c5f1969100d6ae59a2046606
Leomar da Silva Barbosa <leomar.barbosa@kaiostech.com>
2019-08-30 09:47:01 -0400

    update

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,9 +8,21 @@ require_relative "helpers"
 
 World(Helpers)
 
+# case ENV["BROWSER"]
+
+# when "firefox"
+#   @driver = :selenium
+# when "chrome"
+#   @driver = :selenium_chrome
+# when "headless"
+#   @driver = :selenium_chrome_headless
+# else
+#    puts "Invalid Browser"
+# end       
+
 Capybara.configure do |config|
   #config.default_driver = :selenium
-  config.default_driver = :selenium_chrome#_headless
+  config.default_driver = :selenium_chrome
   config.app_host = "localhost:8080/login"
   #config timeout implicit
   config.default_max_wait_time = 10
