6a127fb6cabaa2691e184140ffc6f83c2178af90
Leomar da Silva Barbosa <leomar.barbosa@kaiostech.com>
2019-09-03 12:13:46 -0400

    update

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,20 +1,22 @@
 pipeline {
     agent {
         docker {
-            image "ruby"
+            image "ruby:alpine"
         }
        
     }
-
     stages {
         stage("Build") {
             steps {
-                sh "echo 'simulando um build'"
+                sh "chmod +x build/alpine.sh"
+                sh "./build/alpine.sh"
+                sh "gem install bundler:2.0.2"
+                sh "bundle install"
             }
         }
         stage("Testes"){
             steps {
-                sh "bundle exec cucumber'"
+                sh "bundle exec cucumber -p ci"
             }
         }
     }
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,24 +1,36 @@
-require "capybara"
-require "capybara/cucumber"
-require "selenium-webdriver"
-require "rspec"
-require "os"
+require 'capybara'
+require 'capybara/cucumber'
+require 'capybara/rspec'
+require 'selenium-webdriver'
 require 'report_builder'
 require 'allure-cucumber'
 
-require_relative "helpers"
-
-World(Helpers)
-
-
+#configuração de ambientes
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_TYPE"]}.yaml"))
 
+#configuração para execução de browsers
+    case ENV["BROWSER"]
+    when "firefox"
+        @driver = :selenium
+    when "chrome"
+        @driver = :selenium_chrome
+    when "headless"
+        Capybara.register_driver :selenium_chrome_headless do |app|
+            chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |options|
+                options.add_argument "--headless"
+                options.add_argument "--disable-gpu"
+                options.add_argument "--no-sandbox"
+                options.add_argument "--disable-site-isolation-trials"
+            end
+            Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+        end
+        @driver = :selenium_chrome_headless
+    else
+        puts "Invalid Browser"
+    end        
 
 Capybara.configure do |config|
-  #config.default_driver = :selenium
-  config.default_driver = :selenium_chrome
-  config.app_host = CONFIG["url"]
-  #config timeout implicit
-  config.default_max_wait_time = 10
-end    
-
+  config.default_driver = @driver#:selenium_chrome#_headless
+  config.default_max_wait_time = 20
+  config.app_host = CONFIG["url"]    
+end 
